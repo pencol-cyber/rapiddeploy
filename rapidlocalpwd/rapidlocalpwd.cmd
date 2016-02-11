3f7c373abab99af266d9bb3cc50bbfff7ed1c55a
pencol-ccdc <vanguarded@gmail.com>
2016-02-10 20:43:23 -0800

    Fixup for unquoted strings

--- rapidlocalpwd/rapidlocalpwd.cmd
+++ rapidlocalpwd/rapidlocalpwd.cmd
@@ -10,11 +10,13 @@ echo   rrr   rrr   ddd  ddd                                  bofh@pencol.edu  ##
 echo   rrr         dddd ddd                                                   ##\
 echo   rrr          ddddddd                                                   ##\
 echo   rrr
-del userlist.txt list.txt
+if exist userlist.txt (del userlist.txt)
+if exist list.txt (del list.txt)
 timeout 5
 cls
 echo This modules will change the authentication password for every local account to
 echo a new password that you specify
+echo[
 echo Please enter the password:
 set /P newpass01=
 echo[
@@ -47,9 +49,12 @@ net user %1 | findstr /C:"Account Active"
 net user %1 | findstr /C:"Local Group Memberships"
 net user %1 | findstr /C:"Password last set"
 net user %1 | findstr /C:"Comment"
-@echo.       
-echo Changing %1's password to [ %newpass01% ]
-net user %1 %newpass01% /comment:Password changed by Rapiddeploy PRCCDC Toolkit at %TIME% on %DATE%
+@echo[       
+echo [!] Changing %1's password to [ %newpass01% ]
+echo net user %1 %newpass01% /comment:"Password changed by Rapiddeploy PRCCDC Toolkit at %TIME% on %DATE%"
+echo .... Updated information for %1
+net user %1 | findstr /C:"Password last set"
+net user %1 | findstr /C:"Comment"
 echo "[!] .... Done"
 @echo[     
 goto :eof
